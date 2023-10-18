0e3ad505a2ed267a6120c2d38108f59c6a8ea453
Wener Rodrigo <wener19.wr@gmail.com>
2023-10-18 18:18:12 -0300

    definido tema global na aplicação

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/screens/Home';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
 
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
       <Home />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/index.ts
@@ -0,0 +1,18 @@
+export default {
+    colors: {
+        white: "#FFFFFF",
+
+        yellow: "#E1B12C",
+
+    },
+    font_family: {
+        regular: "Roboto_400Regular",
+        bold: "Roboto_700Bold",
+    },
+    font_size: {
+        sm: 14,
+        md: 16,
+        lg: 18,
+        xl: 24,
+    },
+};
