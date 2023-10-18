3b35f99f3bd76771dc8b3de3343ca11aa4ad02ab
Wener Rodrigo <wener19.wr@gmail.com>
2023-10-18 18:07:36 -0300

    criado tela home

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { Home } from './src/screens/Home';
 
 
 export default function App() {
   return (
-    <View>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <Home />
+    </>
   );
 }
 
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles';
+
+export function Home() {
+    return (
+        <S.Container>
+            <S.Title>Olá, React Native</S.Title>
+        </S.Container>
+    );
+}
+
+
+
