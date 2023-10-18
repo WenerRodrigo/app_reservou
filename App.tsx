e18522280b52ff4e6ac4568001000ffb6c21bc04
Wener Rodrigo <wener19.wr@gmail.com>
2023-10-18 18:38:50 -0300

    criado fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -2,14 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { Home } from './src/screens/Home';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/theme';
-
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [FontsLoader] = useFonts([Roboto_400Regular, Roboto_700Bold]);
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
-      <Home />
+      {FontsLoader ? <Home /> : <ActivityIndicator />}
     </ThemeProvider>
+
   );
 }
 
