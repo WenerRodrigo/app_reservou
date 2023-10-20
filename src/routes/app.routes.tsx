b0f9caabf759c800ae0c9e242c39d2058644dee1
WenerRodrigo <wener19.wr@gmail.com>
2023-10-19 21:55:03 -0300

    adicionado imgs na aplicação , e feito a instalação de packages

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,9 +3,20 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Home } from "../screens/Home";
 import { SignUp } from "../screens/SignUp";
 import theme from "../theme";
-
+import { View } from 'react-native';
+import LogoutButton from "../components/LogoutButton";
 
 const Drawer = createDrawerNavigator();
+
+const customDrawerContent = () => {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 8 }}
+        >
+            <LogoutButton />
+        </View>
+    );
+}
 
 
 export const AppRoutes: React.FC = () => {
@@ -16,7 +27,10 @@ export const AppRoutes: React.FC = () => {
                 backgroundColor: theme.colors.yellow,
                 height: 120
             }
-        }}>
+
+        }}
+            drawerContent={customDrawerContent}
+        >
             <Drawer.Screen name="Home" component={Home} />
             <Drawer.Screen name="SignUp" component={SignUp} />
         </Drawer.Navigator>
--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { SignIn } from "../screens/SignIn";
+import { SignUp } from "../screens/SignUp";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ export const AuthRoutes: React.FC = () => {
             }
         }}>
             <Stack.Screen name="SignIn" component={SignIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
         </Stack.Navigator>
     );
 }
