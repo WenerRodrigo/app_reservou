f3bb322bdfca2fed2353c2a057348e1da985ec5a
WenerRodrigo <wener19.wr@gmail.com>
2023-10-20 00:47:29 -0300

    criado um console na tela de login , e corrigido bugs na aplicação

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,10 +3,16 @@ import * as S from './styles';
 import { Button } from '../../components/Button';
 import { useNavigation } from '@react-navigation/native'
 import { Input } from '../../components/Input';
-import { Image } from 'react-native';
+import { Image, Keyboard, ScrollView, TouchableWithoutFeedback, View } from 'react-native';
 import * as yup from 'yup';
 import { Controller, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+
+
+type IFormData = {
+    email: string;
+    password: string;
+}
 
 
 export function SignIn() {
@@ -22,62 +28,80 @@ export function SignIn() {
         resolver: yupResolver(schema)
     })
 
-    const handleSignUP = () => {
+    const handleSignUp = () => {
         navigation.navigate("SignUp")
     }
 
-    return (
-        <S.Container>
-            <S.Form>
-                <Image
-                    source={require('../../../assets/icon.png')}
-                    style={{ width: '40%', height: '40%' }}
-                    resizeMode='contain'
-                />
-                <S.WapperInput>
-                    <S.Title>E-mail</S.Title>
-                    <Controller
-                        name="email"
-                        defaultValue=""
-                        control={control}
-                        render={({ field: { onChange, value } }) => (
-                            <Input
-                                placeholder="E-mail"
-                                keyboardType="email-address"
-                                autoCorrect={false}
-                                autoCapitalize="none"
-                                value={value}
-                                onChangeText={onChange}
-                                error={errors.email && errors.email.message}
-                            />
-                        )}
-                    />
-                </S.WapperInput>
+    const handleFormSubmit = (data: IFormData) => {
 
-                <S.WapperInput>
-                    <S.Title>Senha</S.Title>
-                    <Controller
-                        name="password"
-                        defaultValue=""
-                        control={control}
-                        render={({ field: { onChange, value } }) => (
-                            <Input
-                                placeholder="Senha"
-                                secureTextEntry
-                                autoCorrect={false}
-                                autoCapitalize="none"
-                                value={value}
-                                onChangeText={onChange}
-                                error={errors.password && errors.password.message}
-                            />
-                        )}
+        console.log('Salvando Dados', data);
+
+        signIn();
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <S.Container>
+                <S.BoxImage>
+                    <Image
+                        source={require('../../../assets/icon.png')}
+                        style={{ width: 200, height: 130, marginBottom: 40 }}
+                        resizeMode='contain'
                     />
-                    <S.Title>Esqueceu sua senha ? Clique aqui</S.Title>
-                </S.WapperInput>
-            </S.Form>
-            <Button title="Entrar" onPress={handleSubmit(signIn)} />
-            <Button title="Criar Conta" type='secondary' onPress={handleSignUP} />
-        </S.Container>
+                </S.BoxImage>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    alwaysBounceVertical={false}
+                >
+                    <S.WapperInput>
+                        <S.Title>E-mail</S.Title>
+                        <Controller
+                            name="email"
+                            defaultValue=""
+                            control={control}
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    placeholder="E-mail"
+                                    keyboardType="email-address"
+                                    autoCorrect={false}
+                                    autoCapitalize="none"
+                                    value={value}
+                                    onChangeText={onChange}
+                                    error={errors.email && errors.email.message}
+                                />
+                            )}
+                        />
+                    </S.WapperInput>
+
+                    <S.WapperInput>
+                        <S.Title>Senha</S.Title>
+                        <Controller
+                            name="password"
+                            defaultValue=""
+                            control={control}
+                            render={({ field: { onChange, value } }) => (
+                                <Input
+                                    placeholder="Senha"
+                                    autoCorrect={false}
+                                    autoCapitalize="none"
+                                    value={value}
+                                    onChangeText={onChange}
+                                    type='password'
+                                    error={errors.password && errors.password.message}
+                                />
+                            )}
+                        />
+                        <S.Title>Esqueceu sua senha ? Clique aqui</S.Title>
+                    </S.WapperInput>
+                    <S.BoxButtom>
+                        <Button title="Entrar" onPress={handleSubmit(handleFormSubmit)} />
+                    </S.BoxButtom>
+                    <S.BoxButtom>
+                        <Button title="Criar Conta" type='secondary' onPress={handleSignUp} />
+                    </S.BoxButtom>
+                </ScrollView>
+            </S.Container>
+        </TouchableWithoutFeedback>
     );
 }
 
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -7,25 +7,25 @@ export const Container = styled(SafeAreaView)`
     padding: 24px;
 `;
 
+export const BoxImage = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+`;
+
 export const Title = styled.Text`
     font-size: 16px;
     font-weight: 400;
     color: ${({ theme }) => theme.colors.black};
 `;
 
-export const Form = styled.View`
-    width: 100%;
-    align-items: center;
-`;
-
 export const WapperInput = styled.View`
     margin-bottom: 10px;
     width: 100%;
-    margin-top: 25px;
 `;
 
 
 export const BoxButtom = styled.View`
-    margin-top: 25px;
-    
+    margin-top: 10px;
 `
